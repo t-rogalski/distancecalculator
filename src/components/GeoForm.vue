95fd5be49b1fb7b004d1181cd2721817395a46b5
t-rogalski <tomaszrogalski04@gmail.com>
2025-06-06 17:02:46 +0200

    Poprawa wymagań do inputów w formularzu
    Współrzędne mogą być ujemne

--- src/components/GeoForm.vue
+++ src/components/GeoForm.vue
@@ -3,19 +3,19 @@
     <div class="coords">
       <div class="input">
         <label>Punkt A</label>
-        <input v-model.number="lat1" type="number" step="any" min="0" max="90" placeholder="Szerokość" required />
+        <input v-model.number="lat1" type="number" step="any" min="-90" max="90" placeholder="Szerokość" required />
       </div>
       <div class="input">
-        <input v-model.number="lon1" type="number" step="any" min="0" max="180" placeholder="Długość" required />
+        <input v-model.number="lon1" type="number" step="any" min="-180" max="180" placeholder="Długość" required />
       </div>
     </div>
     <div class="coords">
       <div class="input">
         <label>Punkt B</label>
-        <input v-model.number="lat2" type="number" step="any" min="0" max="90" placeholder="Szerokość" required />
+        <input v-model.number="lat2" type="number" step="any" min="-90" max="90" placeholder="Szerokość" required />
       </div>
       <div class="input">
-        <input v-model.number="lon2" type="number" step="any" min="0" max="180" placeholder="Długość" required />
+        <input v-model.number="lon2" type="number" step="any" min="-180" max="180" placeholder="Długość" required />
       </div>
     </div>
     <button type="submit">Oblicz odległość</button>
