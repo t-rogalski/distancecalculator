2c565a7d5f3b5f06a02f256fc9119bfe4d178be7
t-rogalski <tomaszrogalski04@gmail.com>
2025-06-06 16:35:57 +0200

    Navbar bez media

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,68 @@
 <template>
   <nav>
-    <div class="navup">
-      <div>
-        <p>Infolinia:</p>
-        <a href="#">(22) 19654 </a>
-      </div>
-      <p>Ococho</p>
+    <div class="logo">
+      <a class="navbar-brand" href="https://ikol.pl/">
+        <img src="/logo-ikol1.png" alt="LOGO" />
+        <p>OCHRONA POJAZDÓW</p>
+      </a>
     </div>
-    <div class="navdown">
-      <div class="navbar-brand">Kalkulator Odległości</div>
-      <ul class="navbar-links">
-        <li><a href="#">Strona główna</a></li>
-        <li><a href="#">O projekcie</a></li>
-        <li><a href="#">Kontakt</a></li>
-      </ul>
+    <div class="navbar">
+      <div class="navup">
+        <div>
+          <p>Infolinia:</p>
+          <a href="#">(22) 19654 </a>
+        </div>
+        <a href="#">ZALOGUJ SIĘ</a>
+        <button class="signup">ZAŁÓŻ KONTO</button>
+      </div>
+      <div class="navdown">
+        <ul class="navbar-links">
+          <li class="dropdown">
+            <a href="#">Radiowe zabezpieczenie - SVR</a>
+            <ul class="dropdown-menu dropdown-menu-svr">
+              <li><a href="#">Opis systemu radiowego</a></li>
+              <li><a href="#">Oferta</a></li>
+              <li><a href="#">IKOL SVR Dashcam - Nowość!</a></li>
+            </ul>
+          </li>
+          <li class="dropdown">
+            <a href="#">Lokalizatory GPS</a>
+            <ul class="dropdown-menu dropdown-menu-gps">
+              <li><a href="#">GPS do samochodu</a></li>
+              <li><a href="#">GPS dla flot</a></li>
+              <li><a href="#">Lokalizatory przenośne</a></li>
+              <li><a href="#">e-TOLL</a></li>
+              <li><a href="#">Lokalizacja telefonu</a></li>
+              <li><a href="#">Oferta</a></li>
+            </ul>
+          </li>
+          <li class="dropdown">
+            <a href="#">Baza wiedzy</a>
+            <ul class="dropdown-menu dropdown-menu-knowledge">
+              <li><a href="#">Niezagłuszalny system IKOL SVR</a></li>
+              <li><a href="#">Moduły IKOL dla pojazdów</a></li>
+              <li><a href="#">Lokalizatory IKOL przenośne</a></li>
+              <li><a href="#">Nasze odzyskania</a></li>
+              <li class="dropdown-submenu">
+                <a href="#">Nasze aplikacje</a>
+                <ul class="submenu">
+                  <li><a href="#">Aplikacja IKOL Tracer</a></li>
+                  <li><a href="#">Aplikacja IKOL X</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Usługi dodatkowe</a></li>
+              <li><a href="#">Nasze rozwiązania</a></li>
+              <li><a href="#">Lista pojazdów CAN (marki A do L)</a></li>
+              <li><a href="#">Lista pojazdów CAN (marki M do Z)</a></li>
+              <li><a href="#">Filmy</a></li>
+              <li><a href="#">FAQ</a></li>
+              <li><a href="#">API IKOL</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Kontakt</a></li>
+        </ul>
+      </div>
     </div>
   </nav>
 </template>
